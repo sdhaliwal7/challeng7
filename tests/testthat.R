30bf3793cbdf2fb1be64033255df57e5effd5d58
sdhaliwal7 <67290557+sdhaliwal7@users.noreply.github.com>
2020-08-09 23:01:52 -0700

    i dont know if we need tests as well?

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,16 +1,6 @@
 library(testthat)
 library(twelvedays)
 
-test_check("twelvedays")
+test_check("challenge7")
 
-test_that('ten  lords a-leaping',{
-   make_phrase(num = 10,num_word ="ten", item = "lords",verb = "a-leaping", adjective = "", location = "")
-  })
 
-test_that("rings", {
-  pluralize_gift("ring")
-  })
-
-test_that("making song", {
-  sing_day(iteration, 1, Full.Phrase, Day.in.Words)
-  })
